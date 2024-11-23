34385448ffe400cdaa305c448265671fb40a262d
Rafael dos Santos <rafoliael@gmail.com>
2024-11-23 01:02:15 -0300

    aula 3

--- server.js
+++ server.js
@@ -1,30 +1,9 @@
 import express from "express";
-
-const posts = [
-    { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
-    { id: 2, descricao: "Gato fazendo yoga", imagem: "https://placecats.com/millie/300/150" },
-    { id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150"},
-];
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-
-app.use(express.json());
+routes(app)
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
-});
-
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
-});
-
-function buscarPostPorID(id) {
-    return posts.findIndex((post) => {
-        return post.id === Number(id)
-    })
-}
-
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPostPorID(req.params.id)
-    res.status(200).json(posts[index]);
 });
