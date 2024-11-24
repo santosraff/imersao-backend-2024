45d9ee439066e5086a51478aa2be831187745124
Rafael dos Santos <rafoliael@gmail.com>
2024-11-24 00:00:50 -0300

    aula 5

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import conectarAoBanco from "../config/dbConfig.js"
 
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
@@ -12,4 +13,11 @@ export async function criarPost(novoPost) {
     const db = conexao.db("imersao-instabytes");
     const colecao = db.collection("posts");
     return colecao.insertOne(novoPost)
+}
+
+export async function atualizarPost(id, novoPost) {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    const objectId = ObjectId.createFromHexString(id);
+    return colecao.updateOne({_id: new ObjectId(objectId)}, {$set: novoPost})
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,7 +1,12 @@
 import express from "express";
 import multer from "multer";
-import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+import cors from "cors";
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js";
 
+const corsOption = {
+    origin: "http//localhost:8000",
+    optionsSuccessStatus: 200
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -16,10 +21,12 @@ const upload = multer({ storage: storage });
 
 const routes = (app) => {
     app.use(express.json());
-
+    app.use(cors(corsOption));
+    
     app.get("/posts", listarPosts); 
     app.post("/posts", postarNovoPost)
     app.post("/upload", upload.single("imagem"), uploadImagem);
+    app.put("/upload/:id", atualizarNovoPost);
 };
 
 export default routes;
